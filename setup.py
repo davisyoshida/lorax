c88020c324ffc848191bbc496ef184da89e1ff67
Davis <davis.yoshida@gmail.com>
2023-05-02 21:34:37 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='lorax',
+    version='0.0.1',
+    author='Davis Yoshida',
+    author_email='davis.yoshida@gmail.com',
+    description='JAX implementation of LoRA',
+    long_description='.',
+    long_description_content_type='text',
+    url=None,
+    packages=['lorax'],
+)
